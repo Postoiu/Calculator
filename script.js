02dfd5a5bf3e99f1a3b52a78b9cfbf20e2306487
Ovidiu Postoiu <postoiu.o@gmail.com>
2021-03-03 16:45:38 +0200

    Create add, sub, multiply, divide and operate functions

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 
+
+
+function operate(a, b, operator) {
+    return operator === '+' ? add(a,b):
+            operator === '-' ? sub(a,b):
+            operator === '*' ? multiply(a,b):
+            operator === '/' ? divide(a,b): 'Unknown operator!';
+    
+}
+
+const add = (a, b) => a + b;
+const sub = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
