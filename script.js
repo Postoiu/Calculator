f32f7b9a0c58dd9664dc99716b683ee531fff14d
Ovidiu Postoiu <postoiu.o@gmail.com>
2021-03-04 16:13:20 +0200

    Added a decimal point button and updated the script to work with decimal point

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const calculator = {
 }
 
 let input1, operator, input2, result;
+let count = 0;
 
 calculator.keys.forEach(key => {
     key.addEventListener('click', () => {
@@ -17,16 +18,47 @@ calculator.keys.forEach(key => {
             }
         }
         if(key.classList.contains('operator')) {
+            count += 1;
+            if(count > 1) {
+                input2 = calculator.display.value.slice(calculator.display.value.indexOf(operator) + 1);
+                result = operate(input1, input2, operator);
+
+                if(result.toString() === 'Infinity') {
+                    calculator.display.value = 'Can not divide by 0';
+                } else if(result.toString().indexOf('.') === -1) {
+                    calculator.display.value = result.toString();
+                } else {
+                    calculator.display.value = result.toFixed(2).toString();
+                }
+            }
+
             input1 = calculator.display.value;
             calculator.display.value += key.value;
             operator = key.value;
         }
         if(key.classList.contains('equal-sign')) {
-            input2 = calculator.display.value.slice(
-                calculator.display.value.indexOf(operator) + 1
-            );
+            input2 = calculator.display.value.slice(calculator.display.value.indexOf(operator) + 1);
             result = operate(input1, input2, operator);
-            calculator.display.value = result.toString();
+
+            if(result.toString() === 'Infinity') {
+                calculator.display.value = 'Can not divide by 0';
+            } else if(result.toString().indexOf('.') === -1) {
+                calculator.display.value = result.toString();
+            } else {
+                calculator.display.value = result.toFixed(2).toString();
+            }
+
+            count = 0;
+        }
+        if(key.classList.contains('all-clear')) {
+            calculator.display.value = '0';
+            count = 0;
+            document.getElementsByClassName('decimal')[0].removeAttribute('disabled');
+        }
+
+        if(key.classList.contains('decimal')) {
+            calculator.display.value += key.value;
+            key.setAttribute('disabled', 'true');
         }
     });
 });
